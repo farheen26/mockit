9ad335ea155608d6d84c0c29eb6b83f6cf6fe83e
Farheen.Fathima <farheen.fathima@brillio.com>
2022-11-16 21:58:41 +0530

    test msg

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -237,7 +237,7 @@ const TableDisplay = () => {
                         rows={4}
                         sx={{ marginTop: "15px", width: "400px" }}
                       >
-                        gsadgaskgdaskglag
+                        placeholder
                       </TextField>
                     </Grid>
                   </Grid>
